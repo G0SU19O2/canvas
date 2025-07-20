f2797949e72763f2146ab54611b32d92206d8ae6
G0SU19O2 <huyquocnguyen1902@gmail.com>
2025-07-20 11:57:58 +0700

    Implement server setup and testing framework
    - Added server package with functionality to start and stop an HTTP server.
    - Created integration test utilities for server testing.
    - Updated go.mod and go.sum to include necessary dependencies.
    - Added tests to verify server behavior.

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module canvas
 
 go 1.24
+
+require github.com/go-chi/chi v1.5.5
+
+require github.com/matryer/is v1.4.1 // indirect
--- server/server.go
+++ server/server.go
@@ -1,2 +1,59 @@
 // Package server contains everything for setting up and running the HTTP server.
 package server
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-chi/chi"
+)
+
+type Server struct {
+	address string
+	mux     chi.Router
+	server  *http.Server
+}
+
+type Options struct {
+	Host string
+	Port int
+}
+
+func New(otps Options) *Server {
+	address := net.JoinHostPort(otps.Host, strconv.Itoa(otps.Port))
+	mux := chi.NewMux()
+	httpServer := &http.Server{
+		Addr:              address,
+		Handler:           mux,
+		ReadTimeout:       5 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+		IdleTimeout:       5 * time.Second,
+	}
+	return &Server{address: address, mux: mux, server: httpServer}
+}
+
+
+func (s *Server) Start() error {
+	s.setupRoutes()
+	fmt.Println("Starting on", s.address)
+	if err := s.server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("error starting server: %w", err)
+	}
+	return nil
+}
+
+func (s *Server) Stop() error {
+	fmt.Println("Stopping")
+	ctx, cancel := context.WithTimeout(context.Background(), 30 * time.Second)
+	defer cancel()
+	if err := s.server.Shutdown(ctx); err != nil {
+		return fmt.Errorf("error stopping server: %w", err)
+	}
+	return nil
+}
